c47caefda05301816deeecc927c3daf8902f7b62
edmaputra <bangun.edma.saputra@gmail.com>
2020-04-11 22:53:00 +0700

    Add Person entity

---
+++ core/src/main/java/io/github/edmaputra/ed/core/model/Gender.java
@@ -0,0 +1,9 @@
+package io.github.edmaputra.ed.core.model;
+
+public enum Gender {
+
+    MALE,
+    FEMALE,
+    UNKNOWN
+
+}
---
+++ core/src/main/java/io/github/edmaputra/ed/core/model/MaritalStatus.java
@@ -0,0 +1,10 @@
+package io.github.edmaputra.ed.core.model;
+
+public enum MaritalStatus {
+
+    SINGLE,
+    MARRIED,
+    WIDOWER,
+    DIVORCEE
+
+}

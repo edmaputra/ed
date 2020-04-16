9e418aecc6f2f5b871608af3eb9322f21dc53bd3
edmaputra <bangun.edma.saputra@gmail.com>
2020-04-16 23:08:46 +0700

    Add custom and specific exception

---
+++ data/src/main/java/io/github/edmaputra/ed/data/exception/DataEmptyException.java
@@ -0,0 +1,15 @@
+package io.github.edmaputra.ed.data.exception;
+
+/**
+ * Data Empty Exception
+ *
+ * @author edmaputra
+ * @since 0.0.1
+ */
+public class DataEmptyException extends Exception {
+
+  public DataEmptyException(String message) {
+    super(message);
+  }
+
+}
---
+++ data/src/main/java/io/github/edmaputra/ed/data/exception/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package io.github.edmaputra.ed.data.exception;
+
+/**
+ * Data Empty Exception
+ *
+ * @author edmaputra
+ * @since 0.0.1
+ */
+public class DataNotFoundException extends Exception {
+
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+
+}
